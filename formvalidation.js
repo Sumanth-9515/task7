aa3a04d47cff8afe8e39fde9bb2f5cc2e2ee65e2
Sumanth-9515 <myakalasumanthreddy@gmail.com>
2024-11-24 12:14:55 +0530

    Add files via upload

---
+++ formvalidation.js
@@ -0,0 +1,11 @@
+
+
+    document.getElementById("siginup").addEventListener("submit",(event)=>{
+        event.preventDefault()
+    let usernamevalid= /^[a-zA-Z]{3,}$/
+    let emailvalid=/^[a-zA-Z0-9.+_%$#]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    // let pswdvalid = /^(?=(.*[a-z]){2,})(?=(.*[A-Z]){2,})(?=(.*\d){2,})(?=(.*[!@#$%^&*()_+]){2,})[a-zA-Z0-9!@#$%^&*()_+]{8,15}$/
+
+    )}
+
+
